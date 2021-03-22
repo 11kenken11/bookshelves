1be60e18c90a4db83e22d7f8085741e8b2ffdfcf
11kenken11 <kenji111.fujimoto@gmail.com>
2021-03-22 12:07:15 +0100

    angular firebase project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyC6kjm42fR3MmUyI0WtD47v7T36am1m70w",
+      authDomain: "bookshelves-8f274.firebaseapp.com",
+      projectId: "bookshelves-8f274",
+      storageBucket: "bookshelves-8f274.appspot.com",
+      messagingSenderId: "675587917385",
+      appId: "1:675587917385:web:d5ec95e4e216428c462198"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
